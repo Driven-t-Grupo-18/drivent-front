595e5b6455ea6b296e90fc63e34cb105de8d4bff
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-10-21 00:01:48 -0300

    feat: config payment page

--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -1,24 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PaymentOptions from "../../../components/PaymentProcess";
 import PaymentForm from '../../../components/PaymentForm';
+import { useGetTicket } from '../../../hooks/api/useTicket';
+import styled from 'styled-components';
 
 export default function Payment() {
   const [status, setStatus] = useState("pending");
+  const [ticket, setTicket] = useState(undefined);
+  const { getTicket } = useGetTicket();
 
-  const myObject = {
-    id: 3,
-    includesHotel: true,
-    isRemote: false,
-    price: 10
-  };
+  useEffect( () => {
+    const data = async () => {
+      const ticket = await getTicket();
+      if (ticket && status !== "finished") setStatus("finished");
+    }
 
-  const [ticket, setTicket] = useState(myObject);
+    data();
+
+  }, [status]);
   
   return (
     <>
-      
-        <PaymentForm ticket={ticket} />
-      
+      {status === "pending" && <PaymentOptions setStatus={setStatus} setTicketType={setTicket} /> }
+      {status === "payment" && <PaymentForm ticketType={ticket} /> }      
+      {status === "finished" && 
+      <Finish>
+        <h1>Pagamento concluído.</h1>
+      </Finish>}      
     </>   
   );
 }
+
+const Finish = styled.div`
+  text-align: center;
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    color="#8E8E8E"
+  }
+`;
