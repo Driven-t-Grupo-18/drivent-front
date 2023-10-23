55972866e3e8fcdb6dd435e06efdb494237dbc8d
Brendo <brendow_prime@hotmail.com>
2023-10-23 13:50:26 -0400

    update paymant

---
+++ src/components/PaymentProcess/incompleteRegistration.jsx
@@ -0,0 +1,23 @@
+import { styled } from "styled-components";
+
+export default function IncompleteRegistration() {
+
+    return (
+        <>
+            <StyledDiv>
+                <h1>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</h1>
+            </StyledDiv>
+        </>
+    );
+}
+
+const StyledDiv = styled.div`
+    h1{
+        font-size: 20px;
+        font-weight: 400;
+        line-height: 23px;
+        letter-spacing: 0em;
+        text-align: center;
+        color: #8E8E8E;
+    }
+`
--- src/pages/Dashboard/Payment/index.jsx
+++ src/pages/Dashboard/Payment/index.jsx
@@ -4,6 +4,7 @@ import PaymentForm from '../../../components/PaymentForm';
 import { useGetTicket } from '../../../hooks/api/useTicket';
 import useToken from '../../../hooks/useToken';
 import axios from 'axios';
+import IncompleteRegistration from '../../../components/PaymentProcess/incompleteRegistration';
 
 export default function Payment() {
   const token = useToken()
@@ -11,22 +12,32 @@ export default function Payment() {
   const [ticketType, setTicketType] = useState(undefined);
   const [ticket, setTicket] = useState();
   useEffect( () => {
-    axios.get(`${import.meta.env.VITE_API_URL}/tickets`, {headers: {Authorization: `Bearer ${token}`}})
-    .then( res => {
-        setTicketType(res.data.TicketType)
-        setStatus('payment')
-        setTicket(res.data)
-    })
+
+    axios.get(`${import.meta.env.VITE_API_URL}/enrollments`, {headers: {Authorization: `Bearer ${token}`}})
+    .then( () => {
+      axios.get(`${import.meta.env.VITE_API_URL}/tickets`, {headers: {Authorization: `Bearer ${token}`}})
+      .then( res => {
+          setTicketType(res.data.TicketType)
+          setStatus('payment')
+          setTicket(res.data)
+      })
+      .catch(err => {
+        if (err.response?.status === 404) return setStatus('pending')
+        console.error(err)
+        setStatus('pending')
+      })
+        if (ticketType && status !== "finished") setStatus("finished");
+    }
+    )
     .catch(err => {
-      if (err.response?.status === 404) return setStatus('pending')
       console.error(err)
-      setStatus('pending')
+      setStatus('incomplete')
     })
-      if (ticketType && status !== "finished") setStatus("finished");
   }, [token]);
   
   return (
     <>
+      {status === "incomplete" && <IncompleteRegistration /> }
       {status === "pending" && <PaymentOptions setStatus={setStatus} setTicket={setTicketType} /> }
       {status === "payment" && <PaymentForm ticket={ticket} ticketType={ticketType} /> }      
     </>   
