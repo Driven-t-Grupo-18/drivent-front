17b08ddc4845ab01020318606b4e2aeb2dc9deb7
vitoriacurty <vitoria.curty@hotmail.com>
2023-10-27 15:49:36 -0300

    reactor component button

--- src/components/Form/Button.jsx
+++ src/components/Form/Button.jsx
@@ -1,10 +1,27 @@
 import MuiButton from '@mui/material/Button';
 import styled from 'styled-components';
+import useToken from '../../hooks/useToken';
+import api from '../../services/api';
 
-export default function Button({ variant='contained', children, ...props }) {
+export default function Button({ variant = 'contained', weekDay, date, setActivities, ...props }) {
+  const token = useToken();
+  let isoDate = '';
+  if (date === '10/05') isoDate = '2023-05-10';
+  if (date === '11/05') isoDate = '2023-05-11';
+  if (date === '12/05') isoDate = '2023-05-12';
+
+  async function filterActivities() {
+    const activitiesFilter = await api.get(`/activities/${isoDate}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setActivities(activitiesFilter.data);
+  }
+
   return (
-    <StyledMuiButton variant={variant} {...props}>
-      {children}
+    <StyledMuiButton variant={variant} {...props} onClick={filterActivities}>
+      {weekDay}, {date}
     </StyledMuiButton>
   );
 }
