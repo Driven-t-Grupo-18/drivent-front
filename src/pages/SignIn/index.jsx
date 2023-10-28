20ef8de8d3934a060185c8430ee89f89e1ee8830
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-10-28 15:47:31 -0300

    feat: sign-in with github

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -14,6 +14,9 @@ import UserContext from '../../contexts/UserContext';
 
 import useSignIn from '../../hooks/api/useSignIn';
 
+import qs from "query-string";
+import axios from 'axios';
+
 export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -36,7 +39,43 @@ export default function SignIn() {
     } catch (err) {
       toast('Não foi possível fazer o login!');
     }
-  } 
+  }
+
+  function redirectToGitHub() {
+    const GITHUB_URL = 'https://github.com/login/oauth/authorize';
+    const params = {
+      response_type: 'code',
+      scope: 'user',
+      client_id: process.env.CLIENT_ID,
+      redirect_uri: process.env.REDIRECT_URL
+    }
+    const queryStrings = qs.stringify(params);
+    const authURL = `${GITHUB_URL}?${queryStrings}`;
+    window.location.href = authURL;
+  }
+
+  window.onload = async () => {
+    document.querySelector(".login").addEventListener("click", redirectToGitHub);
+
+    //const urlParams = new URLSearchParams(window.location.search);
+    //const code = urlParams.get("code");
+
+    const { code } = qs.parseUrl(window.location.href).query;
+    if(code) {
+      try {
+        const response = await axios.post(`${process.env.VITE_API_URL}/sign-in`, { code });
+        const user = response.data;
+        console.log(user);
+        //const { token } = response.data;
+        //localStorage.setItem("token", token);
+        //window.location.href = "/profile.html"
+        
+      } catch (err) {
+        toast('Não foi possível fazer o login!')
+        console.log("err", err);
+      }
+    }
+  }
 
   return (
     <AuthLayout background={eventInfo.backgroundImageUrl}>
@@ -50,6 +89,7 @@ export default function SignIn() {
           <Input label="E-mail" type="text" fullWidth value={email} onChange={e => setEmail(e.target.value)} />
           <Input label="Senha" type="password" fullWidth value={password} onChange={e => setPassword(e.target.value)} />
           <Button type="submit" color="primary" fullWidth disabled={loadingSignIn}>Entrar</Button>
+          <Button color="primary" fullWidth onClick={redirectToGitHub}> Entrar com GitHub</Button>
         </form>
       </Row>
       <Row>
